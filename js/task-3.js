0c264d4ab79b12f0817a127cee62d19e44e7fa5d
Anton L. <blizzbox88@gmail.com>
2024-10-18 16:25:49 +0300

    initials

--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,34 @@
-function checkForSpam(message) {
+const filterArray = function (numbers, value) {
+    
+    let fitArr = [];
 
-    message = message.toLowerCase();
-
-    if (message.includes('spam') || message.includes('sale')) {
-        return true;
-    } else {
-        return false;
-    }
+    for (let index = 0; index < numbers.length; index += 1) {
+    
+        let fitNum = numbers[index];
+ 
+        if (fitNum > value) {
+            fitArr.push(fitNum);
+        }
 }
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+ return fitArr;
+
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+
+
+
+
+
+
+
+
+
+
+
